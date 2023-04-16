042fb0a6d08bd9b7a97f8c59c2a2ffc7d072d1e8
nabilhussein <55296823+nabilhussein@users.noreply.github.com>
2023-04-15 20:51:46 -0400

    Initial commit

---
+++ src/main/java/org/example/cal/BasicCalculator.java
@@ -0,0 +1,13 @@
+package org.example.cal;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args){
+        System.out.println("would u like to add a number ");
+
+        Scanner consoleInput = new Scanner(System.in);
+
+    }
+
+}
---
+++ src/main/java/org/example/cal/Calculator.java
@@ -0,0 +1,26 @@
+package org.example.cal;
+import static java.lang.Math.*;
+import static java.lang.System.*;
+public class Calculator {
+    public static void main(String[] args) {
+
+        int job1Salary = 70000;
+        int job2Salary = 90000;
+
+        System.out.println("This is thr higher paying salary: " + Math.max(job1Salary, job2Salary));
+
+        int truckPrice = 75000;
+        int carPrice = 50000;
+
+        System.out.println("The cheaper car is: " + Math.min(truckPrice , carPrice));
+
+        out.println("The are of the circle is: " + 3.141592653589793238 * pow(7.25, 2));
+
+        double num1 = 5.0;
+        out.println("The square root is " + sqrt(num1));
+
+
+    }
+
+
+}
